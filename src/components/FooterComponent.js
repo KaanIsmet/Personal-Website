e82a80d33092de7e8bef0d73e638c8a3e0e1c4f7
KaanIsmet <kaanismet7@gmail.com>
2023-02-19 01:26:18 -0500

    added footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,10 +1,18 @@
 import React from "react";
-
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container, Col, Row} from "react-bootstrap"
 function FooterComponent() {
-    return(
-        <div></div>
-    )
+    return (
+        <footer className="bg-primary text-light fixed-bottom">
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        © 2023 My Company. All rights reserved.
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
 }
 
 export default FooterComponent
