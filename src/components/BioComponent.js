29cbd5f14bc1fa74377dc92ebc6eccc5b6f4667e
KaanIsmet <kaanismet7@gmail.com>
2023-02-27 01:27:35 -0500

    Added bio page, skills page, and projects page.

---
+++ src/components/BioComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+function BioComponent() {
+    return(
+        <div>
+            Bio page
+        </div>
+    )
+}
+export default BioComponent
